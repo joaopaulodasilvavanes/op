f4cc9955680f90471c3a7e723dbc238e4eab4d4d
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-08-24 17:28:04 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// this is a js file
+// joao paulo
+
+/*
+//1.a. false
+ b. false
+ c. true
+ d.bollean
+
+
+//2. resposta
+ele nao converteu o valor para numero porque esqueceu do comando necessario
+*/
+//3. resposta
+let idade= prompt ("qual ea sua idade?");
+let melhoramigo= prompt ("qual ea sua idade amigo?");
+console.log(("Sua idade é maior do que a do seu melhor amigo"));
+
+
+
+
